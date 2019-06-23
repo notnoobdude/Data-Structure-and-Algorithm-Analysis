a3f0a95a9b8a13b7891c04e2dae5df647c011f9c
Dan Nino <timpaxerror@gmail.com>
2019-06-24 02:07:04 +0800

    Add files via upload

---
+++ 9/InToPostMain.java
@@ -0,0 +1,69 @@
+package stacks2;
+
+/*
+ * 				FABRO, DAN NINO I.							BSCpE2A_C1						COE20						Lab Assignment #5
+ */
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class InToPostMain {
+	
+	static Scanner s = new Scanner(System.in);												//Built-in java class Scanner instantiated b for input.
+	
+	static char option;
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("This program converts an infix arithmetic expression into a postfix expression.");
+		System.out.println("Continue? (y/n): ");
+		option = s.next().charAt(0);	
+			if (option == 'y'||option == 'Y')
+			{
+				userInput();												//Calls out static method userInput() so that implementations inside it will be executed.
+			}
+			
+			else if (option == 'n'||option == 'N')
+			{
+				System.exit(0);
+			}
+			
+			else
+			{
+				System.out.println("Wrong input. Try again.");
+			}
+	}
+
+	
+	public static void userInput() throws IOException {
+		String arithExp;																								//If the string infix input of the user here has a '.', it should be followed by a number. If not
+		String pf;																										//preceded, the generated output is incorrect.
+		InfixtoPostfix b = new InfixtoPostfix();
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.println("Input a string for an arithmetic expression: ");
+		arithExp = s.next();																						//User enters string input of arithmetic expression here. The string input will then be passed into
+		pf = b.convert(arithExp);																				//the instantiated 	InfixtoPostfix class' convert() method. The returned result will be passed back here
+		System.out.println("");																					//and is displayed. The result, however, may have spaces in between the operators and operands.
+		System.out.println("");																					//Hence, operands having two digits or more are not confused along the process.
+		System.out.println("Postfix result: " + pf);												
+		
+		ask();																												//Calls out method ask()
+	}
+	
+	public static void ask() throws IOException {
+		char yesNo;
+		System.out.println("---------------------------------------------------------------------------------------------------");
+		System.out.print("\n\nRetry? (y/n): ");
+		yesNo = s.next().charAt(0);																			//User inputs here.
+    		if ( yesNo == 'y'|| yesNo == 'Y') {                      
+    			userInput();																							//This method is simply tasked for the user if he or she still wants to continue or not.
+    		}																													//If input is y or Y, user is directed back to userInput() method. However, if n or N, 
+    		else if (yesNo == 'n'|| yesNo == 'N') {                 									//terminates the program.
+    			System.exit(0);									
+    		}
+    		else {                                            
+    			System.out.println("Invalid input");
+    		}
+	  }
+}
---
+++ 9/InfixtoPostfix.java
@@ -0,0 +1,103 @@
+package  stacks2;
+
+/*
+ * 				FABRO, DAN NINO I.							BSCpE2A_C1						COE20						Lab Assignment #5
+ */
+
+import java.util.Stack;
+
+public class InfixtoPostfix {
+	
+	Stack<Character> b = new Stack<>();		   
+	
+	    public String convert(String expr) {											//This method scans the input infix expression and converts it into a postfix one.
+		String result = "";																			//The entered string will be passed here.
+		int i;																								
+	
+		b.push('\0');																					
+		for(i=0;i<expr.length();i++) 														//The loop will depend upon the length of the entered string characters. Index i is initialized at zero having
+		{																										//a condition depending on the number of string characters which is then post-incremented.
+			switch(expr.charAt(i)) 
+			{
+			
+				case '(':																						//Once the method scans an opening character within the string input along with the operators, they will be pushed
+																													//into the stack. However, if in the process it arrives a closing character, the stack is peeked if it has the pair open character
+				case '[':																						//and if it has, it is popped and the preceding operator will be passed into the result. On the other hand, operands are directly
+																													//passed into the result.
+				case '{':
+						b.push(expr.charAt(i));
+						break;
+			
+				case ')':
+				
+				case ']':																														
+																																				
+				case '}':																														
+						while(b.peek() != '(' && b.peek() != '[' && b.peek() != '{')
+						{
+								result += " " + Character.toString(b.pop());								
+						}
+						
+						b.pop();
+						
+						break;
+																														//It may be observed that some cases doesn't have any implementations inside. Basically, this is because these 
+				case '+':																							//cases have similar implementations which then falls into the next and into the next case and into the next case
+																														//up until it arrive the case where implementations/statements are available and is having a break statement.
+				case '-':
+				
+				case '*':
+				
+				case '/':
+
+						while(precedence(expr.charAt(i)) <= precedence(b.peek()))
+						{
+								if(result.charAt(result.length() - 1) != ' ')
+								{
+										result += " ";
+								}
+					
+								if(b.peek() != '(' && b.peek() != '[' && b.peek() != '{')
+								{
+										result += b.pop() + " "; 
+								}
+						}
+						
+						b.push(expr.charAt(i));
+						
+						break;
+						
+					default:
+							result += Character.toString(expr.charAt(i)) + " ";
+			}
+		}
+		
+		while(b.peek() != '\0')
+		{
+			result += " " + Character.toString(b.pop());
+		}
+		
+		return result;													//postfix output
+	}
+	
+	private int precedence(char x) {
+		switch(x)
+		{
+			case '\0':																								
+					return 0;											//This method is used to determine the comparison of precedence on operators. Returning value is as observed.
+																	
+			case '+':
+			
+			case '-':
+					return 1;
+			
+			case '*':
+			
+			case '/':
+					return 2;
+			
+			default:
+					return 3;
+		}
+	}
+}
